50cfb5628c7765bc88e5c4a82086354425b9af02
Kazeem Olanipekun <kezyolanipekun@gmail.com>
2016-05-26 13:05:27 +0100

    The Frontend is developed using angularJs  to develop a working calculator.

--- js/calculator.js
+++ js/calculator.js
@@ -1,185 +1,77 @@
-/**
- * Created by SQ05 on 5/24/2016.
- */
 /*
-var app = angular.module('calculator1', []);
-
-app.controller("getCal1", ['$log','$scope', function($log ,$scope){
-
-    /**
-     *
-     *   angular model
-     * @type {{one: number}}
-
-    $scope.calValues= {
-
-        one1:1
-
-
-    };
-
-    /**
-     *
-     *   Click one action
-     *
-
-    $scope.clickOne1 = function(){
-      $log.log('Hello');
-    };
-
-
-}]);
-*/
+ Created by SQ05 on 5/24/2016.
+Initializing a Start Application Called calculators
+ */
 var cal = angular.module('calculator', []);
+/*
+Declaration of Global Variables.
+ */
 var conCat = "";
-cal.controller("getCal",['$log', '$scope', function($log, $scope){
+var count=1;
+var len=0;
+var limit=0;
+
+/*
+This starts the usage of a controller name getCal
+ */
+cal.controller("getCal",['$filter', '$log', '$scope', function($filter, $log, $scope){
 
     $scope.calVal={
-    one:1,
-    two:2,
-    three:3,
-    four:4,
-        five:5,
-        six:6,
-            seven:7,
-            eight:8,
-            nine:9,
-            zero:0,
-            add:"+",
-            subtract: "-",
-            divide:"/",
-            multiply:"*",
-        on:"Switched On",
-            off:"Switched Off",
-            cancel:"Enter Prefered Value",
-        result:''
+        result: '',
+    disable:false
         
     };
+/*
+This clickBs is used to Backspace Input Value by removal on last input
+ */
+    $scope.clickBs=function(){
+      len=conCat.length;
+        limit=len-count;
+        $scope.calVal.result=$filter('limitTo')(conCat,limit,0);
+        conCat=$filter('limitTo')(conCat,limit,0);
+        return false;
+    };
 
+    /*
+     This clickCancel is used to Delete all Input Value and setting the view screen to null
+     */
     $scope.clickCancel=function(){
-        $scope.calVal.result=$scope.calVal.cancel;
+        $scope.calVal.result='';
+        conCat='';
+        return false;
+    };
+
+/*
+This ClickAction is used to switch on and off the Calculator.
+ */
+    $scope.clickAction=function($action){
+        if($action=="On") {
+            $scope.calVal.result="Switched On";
+            $scope.calVal.disable=false;
+        }
+        else if($action=="Off"){
+            $scope.calVal.result="Switched Off";
+            $scope.calVal.disable=true;
+        } else{$scope.calVal.result=calVal.disable;}
+        $log.log($scope.calVal.disable);
         conCat= "";
         return false;
     };
 
-    $scope.clickOn=function(){
-        $scope.calVal.result=$scope.calVal.on;
-        conCat= "";
-        return false;
-    };
+/*
+This ClickNumber is used to Concatenate and display the Input Number on the View Screen
+ */
 
-    $scope.clickOff=function(){
-        $scope.calVal.result=$scope.calVal.off;
-        conCat= "";
-        return false;
-    };
-
- $scope.clickOne=function(){
-  
-       conCat+=$scope.calVal.one;
-     $log.log(conCat);
-     $scope.calVal.result=conCat;
-
-    };
-
-    $scope.clickTwo=function(){
-     
-        conCat+=$scope.calVal.two;
+ $scope.clickNumber=function($val){
+        conCat+=$val;
         $log.log(conCat);
         $scope.calVal.result=conCat;
+
     };
-
-    $scope.clickThree=function(){
-     
-        conCat+=$scope.calVal.three;
-        $log.log(conCat);
-        $scope.calVal.result=conCat;
-    };
-
-    $scope.clickFour=function(){
-     
-        conCat+=$scope.calVal.four;
-        $log.log(conCat);
-        $scope.calVal.result=conCat;
-    };
-
-    $scope.clickFive=function(){
-     
-        conCat+=$scope.calVal.five;
-        $log.log(conCat);
-        $scope.calVal.result=conCat;
-    };
-
-    $scope.clickSix=function(){
-     
-        conCat+=$scope.calVal.six;
-        $log.log(conCat);
-        $scope.calVal.result=conCat;
-    };
-
-    $scope.clickSeven=function(){
-     
-        conCat+=$scope.calVal.seven;
-        $log.log(conCat);
-        $scope.calVal.result=conCat;
-    };
-
-    $scope.clickEight=function(){
-     
-        conCat+=$scope.calVal.eight;
-        $log.log(conCat);
-        $scope.calVal.result=conCat;
-    };
-
-    $scope.clickNine=function(){
-     
-        conCat+=$scope.calVal.nine;
-        $log.log(conCat);
-        $scope.calVal.result=conCat;
-    };
-
-    $scope.clickZero=function(){
-     
-        conCat+=$scope.calVal.zero;
-        $log.log(conCat);
-        $scope.calVal.result=conCat;
-    };
-
-    $scope.clickAdd=function(){
-     
-        conCat+=$scope.calVal.add;
-        $log.log(conCat);
-        $scope.calVal.result=conCat;
-    };
-
-    $scope.clickAdd=function(){
-     
-        conCat+=$scope.calVal.add;
-        $log.log(conCat);
-        $scope.calVal.result=conCat;
-    };
-
-    $scope.clickSub=function(){
-     
-        conCat+=$scope.calVal.subtract;
-        $log.log(conCat);
-        $scope.calVal.result=conCat;
-    };
-
-    $scope.clickDiv=function(){
-     
-        conCat+=$scope.calVal.divide;
-        $log.log(conCat);
-        $scope.calVal.result=conCat;
-    };
-
-    $scope.clickMul=function(){
-        conCat+=$scope.calVal.multiply;
-        $log.log(conCat);
-        $scope.calVal.result=conCat;
-    };
-
-
+    //practising 
+/*
+This ClickAns is used to evaluate and display the result of input String on the screen
+ */
     $scope.clickAns=function(){
         $scope.calVal.result=eval(conCat);
         conCat= "";
